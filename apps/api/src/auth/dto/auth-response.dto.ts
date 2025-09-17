0cbb1b4347eb4e090f14568e50be075a75b29a15
dayann deshayes <dayanndeshayes@gmail.com>
2025-09-17 19:29:51 +0200

    add connexion api

---
+++ apps/api/src/auth/dto/auth-response.dto.ts
@@ -0,0 +1,13 @@
+export class AuthResponseDto {
+  accessToken: string;
+  refreshToken: string;
+  tokenType: string;
+  expiresIn: number;
+  user: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    roles: string[];
+  };
+}
